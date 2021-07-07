2d5e924ef7fcae13261aef421261facf8b55ffb7
smartdeveloper19 <desilearning19@gmail.com>
2021-07-07 11:18:46 +0200

    Initial commit

---
+++ Debug/src/stack_in_cpp.d
@@ -0,0 +1,1 @@
+src/stack_in_cpp.o: ../src/stack_in_cpp.cpp
---
+++ src/stack_in_cpp.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : stack_in_cpp.cpp
+// Author      : 
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "hi" << endl; // prints hi
+	return 0;
+}
